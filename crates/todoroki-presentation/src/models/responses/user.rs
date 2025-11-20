97d758065910e2f4d3c6b60d475625417316eeac
reversed-R <qiu1287shan@gmail.com>
2025-11-20 16:32:45 +0900

    feat: also respond user role when get user self

--- crates/todoroki-presentation/src/models/responses/user.rs
+++ crates/todoroki-presentation/src/models/responses/user.rs
@@ -7,8 +7,26 @@ use todoroki_domain::entities;
 pub struct UserResponse {
     pub id: String,
     pub name: String,
+    pub role: UserRoleResponse,
     pub created_at: String,
     pub updated_at: String,
+}
+
+#[derive(Debug, Clone, Copy, Serialize, ToSchema)]
+pub enum UserRoleResponse {
+    #[serde(rename = "owner")]
+    Owner,
+    #[serde(rename = "contributor")]
+    Contributor,
+}
+
+impl From<entities::user::UserRole> for UserRoleResponse {
+    fn from(value: entities::user::UserRole) -> Self {
+        match value {
+            entities::user::UserRole::Owner => Self::Owner,
+            entities::user::UserRole::Contributor => Self::Contributor,
+        }
+    }
 }
 
 impl From<entities::user::User> for UserResponse {
@@ -16,6 +34,7 @@ impl From<entities::user::User> for UserResponse {
         Self {
             id: value.id().clone().value().as_hyphenated().to_string(),
             name: value.name().clone().value(),
+            role: UserRoleResponse::from(value.role().clone()),
             created_at: value.created_at().clone().value().to_rfc3339(),
             updated_at: value.updated_at().clone().value().to_rfc3339(),
         }
